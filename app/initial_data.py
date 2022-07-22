635b2ac7c025433a239b8163475da169f3769fc2
ekalaya2015 <ekalaya2015@gmail.com>
2022-07-22 23:10:33 +0700

    add pendulum

--- app/initial_data.py
+++ app/initial_data.py
@@ -9,6 +9,7 @@ import asyncio
 from datetime import datetime
 import random
 import string
+import pendulum
 from sqlalchemy import select
 
 from app.core import config, security
@@ -44,17 +45,17 @@ async def main() -> None:
             print("Superuser was created")
         else:
             print("Superuser already exists in database")
-        # devices=['Raspigeek001','Raspigeek003']
-        # invdate=pendulum.now()
-        # for i in range (0,100):
-        #     invnum=str(uuid.uuid4())
-        #     random.shuffle(devices)
-        #     total_value=random.randint(100,999)*100
-        #     tax_value=total_value*11/100
-        #     invdate=invdate.add(minutes=10)
-        #     inv=Invoice(id=str(uuid.uuid4()),invoice_num=invnum,invoice_date=invdate.to_iso8601_string(),device_name=devices[0],username='ekalaya2015@gmail.com',total_value=total_value,tax_value=tax_value,created_at=pendulum.now().to_iso8601_string(),modified_at=pendulum.now().to_iso8601_string())
-        #     session.add(inv)
-        #     await session.commit()
+        devices=['Raspigeek001','Raspigeek003']
+        invdate=pendulum.now()
+        for i in range (0,100):
+            invnum=str(uuid.uuid4())
+            random.shuffle(devices)
+            total_value=random.randint(100,999)*100
+            tax_value=total_value*11/111
+            invdate=invdate.add(minutes=10)
+            inv=Invoice(id=str(uuid.uuid4()),invoice_num=invnum,invoice_date=invdate.to_iso8601_string(),device_name=devices[0],username='ekalaya2015@gmail.com',total_value=total_value,tax_value=tax_value,created_at=pendulum.now().to_iso8601_string(),modified_at=pendulum.now().to_iso8601_string())
+            session.add(inv)
+            await session.commit()
         print("Initial data created")
 
 
