ab65fd13bf3b4c71865c0db99f9bad156e572413
ekalaya2015 <ekalaya2015@gmail.com>
2022-07-23 18:20:39 +0700

    autopep8

--- app/initial_data.py
+++ app/initial_data.py
@@ -18,7 +18,7 @@ from app.core import config, security
 
 # from app.core.session import async_session
 from app.core.session import SessionLocal
-from app.models.model import Role, User,Invoice
+from app.models.model import Role, User, Invoice
 
 
 async def main() -> None:
@@ -26,7 +26,8 @@ async def main() -> None:
     print("Start initial data")
     async with SessionLocal() as session:
         result = await session.exec(
-            select(User).where(User.username == config.settings.FIRST_SUPERUSER_EMAIL)
+            select(User).where(User.username ==
+                               config.settings.FIRST_SUPERUSER_EMAIL)
         )
         user = result.one_or_none()
         if user is None:
@@ -47,16 +48,17 @@ async def main() -> None:
             print("Superuser was created")
         else:
             print("Superuser already exists in database")
-        devices=['Raspigeek001','Raspigeek003']
-        invdate=pendulum.now().subtract(days=5)
-        for i in range (0,18):
-            randominvnum=app.utils.invnum_generator.invnum_generate()
-            invnum=f'INV-{randominvnum}'
+        devices = ['Raspigeek001', 'Raspigeek003']
+        invdate = pendulum.now().subtract(days=5)
+        for i in range(0, 18):
+            randominvnum = app.utils.invnum_generator.invnum_generate()
+            invnum = f'INV-{randominvnum}'
             random.shuffle(devices)
-            total_value:float=random.randint(100,999)*100.0
-            tax_value:float=round(total_value*11.0/111.0,2)            
-            invdate=invdate.add(minutes=10)
-            inv=Invoice(id=str(uuid.uuid4()),invoice_num=invnum,invoice_date=invdate.to_iso8601_string(),device_name=devices[0],username='ekalaya2015@gmail.com',total_value=total_value,tax_value=tax_value,created_at=pendulum.now().to_iso8601_string(),modified_at=pendulum.now().to_iso8601_string())
+            total_value: float = random.randint(100, 999)*100.0
+            tax_value: float = round(total_value*11.0/111.0, 2)
+            invdate = invdate.add(minutes=10)
+            inv = Invoice(id=str(uuid.uuid4()), invoice_num=invnum, invoice_date=invdate.to_iso8601_string(
+            ), device_name=devices[0], username='ekalaya2015@gmail.com', total_value=total_value, tax_value=tax_value, created_at=pendulum.now().to_iso8601_string(), modified_at=pendulum.now().to_iso8601_string())
             session.add(inv)
             await session.commit()
         print("Initial data created")
