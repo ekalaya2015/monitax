b12175efcc06fa18a3462fa6791a87e06b14da9d
ekalaya2015 <ekalaya2015@gmail.com>
2022-07-25 15:05:35 +0700

    fix: email template dir

--- app/core/config.py
+++ app/core/config.py
@@ -84,7 +84,7 @@ class Settings(BaseSettings):
         return v
 
     EMAIL_RESET_TOKEN_EXPIRE_HOURS: int = 48
-    EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/target"
+    EMAIL_TEMPLATES_DIR: str = "/app/app/email-templates/tmpl"
     EMAILS_ENABLED: bool = False
 
     @validator("EMAILS_ENABLED", pre=True)
